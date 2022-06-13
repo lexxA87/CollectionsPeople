8c958b661d971477dbf9bd7026841a1f49740774
lexxA87 <alex.o.nikonov@gmail.com>
2022-06-14 01:08:21 +0400

    Added react-router-dom4

--- src/components/adminDashboard/Dashboard.jsx
+++ src/components/adminDashboard/Dashboard.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { withRouter } from "react-router-dom";
+// import { withRouter } from "react-router-dom";
 import { Box } from "adminjs";
 
 function Dashboard() {
   return <Box>Dashboard</Box>;
 }
 
-export default withRouter(Dashboard);
+// export default withRouter(Dashboard);
+export default Dashboard;
