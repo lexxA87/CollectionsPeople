eb53a1b4b8fbd76ec3d94cb9b23e3a3858b7d838
lexxA87 <alex.o.nikonov@gmail.com>
2022-06-14 02:39:20 +0400

    Added react-router-dom7

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Dashboard from "./components/adminDashboard/Dashboard";
+import Dashboard from "./components/adminDashboard/Dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<h1>React</h1>} />
-        {/* <Route path="/admin" element={<Dashboard />} /> */}
+        <Route path="/admin" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
