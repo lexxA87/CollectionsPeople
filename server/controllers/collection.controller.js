4a4f026803595d67b8af5d39f787175e1a21e179
lexxA87 <alex.o.nikonov@gmail.com>
2022-06-14 23:10:37 +0400

    Added controllers and routes for Collection

--- server/controllers/collection.controller.js
+++ server/controllers/collection.controller.js
@@ -1,8 +1,37 @@
 const Collection = require("../models/Collection");
 
 const handleError = (res, error) => {
-  console.log(error);
-  res.send({ message: "Server error" });
+  res.status(500).send(error.message);
+};
+
+const getCollection = (req, res) => {
+  Collection.findById(req.params.id)
+    .then((post) => res.status(200).json(post))
+    .catch((error) => handleError(res, error));
+};
+
+const getCollections = (req, res) => {
+  Collection.find()
+    .then((cols) => res.status(200).json(cols))
+    .catch((error) => handleError(res, error));
+};
+
+const putCollection = (req, res) => {
+  const { title, description, author } = req.body;
+  const { id } = req.params;
+  Collection.findByIdAndUpdate(
+    id,
+    { title, description, author },
+    { new: true }
+  )
+    .then((post) => res.status(200).json(post))
+    .catch((error) => handleError(res, error));
+};
+
+const deleteCollection = (req, res) => {
+  Collection.findByIdAndDelete(req.params.id)
+    .then(() => res.status(200).json(req.params.id))
+    .catch((error) => handleError(res, error));
 };
 
 const postCollection = (req, res) => {
@@ -20,4 +49,10 @@ const postCollection = (req, res) => {
     .catch((error) => handleError(res, error));
 };
 
-module.exports = { postCollection };
+module.exports = {
+  postCollection,
+  getCollection,
+  getCollections,
+  putCollection,
+  deleteCollection,
+};
--- server/routes/api.collection.routes.js
+++ server/routes/api.collection.routes.js
@@ -1,8 +1,18 @@
 const Router = require("express");
-const { postCollection } = require("../controllers/collection.controller");
+const {
+  postCollection,
+  getCollection,
+  getCollections,
+  putCollection,
+  deleteCollection,
+} = require("../controllers/collection.controller");
 
 const router = new Router();
 
 router.post("/api/collection", postCollection);
+router.get("/api/collections", getCollections);
+router.get("/api/collection/:id", getCollection);
+router.put("/api/collection/:id", putCollection);
+router.delete("/api/collection/:id", deleteCollection);
 
 module.exports = router;
