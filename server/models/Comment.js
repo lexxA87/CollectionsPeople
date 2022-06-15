a05e2deaecdc98504d5682c3cf1c94e023ddf153
lexxA87 <alex.o.nikonov@gmail.com>
2022-06-15 17:57:50 +0400

    Added time created comment and item

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 const { Schema, model, ObjectId } = mongoose;
 
-const Comment = new Schema({
-  text: { type: String, required: true },
-  author: { type: ObjectId, ref: "User", required: true },
-  itemParent: { type: ObjectId, ref: "Item", required: true },
-});
+const Comment = new Schema(
+  {
+    text: { type: String, required: true },
+    author: { type: ObjectId, ref: "User", required: true },
+    itemParent: { type: ObjectId, ref: "Item", required: true },
+  },
+  { timestamps: true }
+);
 
 module.exports = model("Comment", Comment);
--- server/models/ItemCollection.js
+++ server/models/ItemCollection.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 const { Schema, model, ObjectId } = mongoose;
 
-const ItemCollection = new Schema({
-  title: { type: String, required: true },
-  author: { type: ObjectId, ref: "User", required: true },
-  collectionParent: { type: ObjectId, ref: "Collection", required: true },
-  likes: { type: Number, default: 0 },
-  tags: [{ type: ObjectId, ref: "Tag" }],
-  comments: [{ type: ObjectId, ref: "Comment" }],
-});
+const ItemCollection = new Schema(
+  {
+    title: { type: String, required: true },
+    author: { type: ObjectId, ref: "User", required: true },
+    collectionParent: { type: ObjectId, ref: "Collection", required: true },
+    likes: { type: Number, default: 0 },
+    tags: [{ type: ObjectId, ref: "Tag" }],
+    comments: [{ type: ObjectId, ref: "Comment" }],
+  },
+  { timestamps: true }
+);
 
 module.exports = model("ItemCollection", ItemCollection);
