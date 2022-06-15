9252b860d8a52e56529091f751bdd6d70632aa38
lexxA87 <alex.o.nikonov@gmail.com>
2022-06-16 00:57:09 +0400

    Added some classes to Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { Navbar, Container, Form, FormControl, Button } from "react-bootstrap";
+import {
+  Navbar,
+  Container,
+  Form,
+  FormControl,
+  Button,
+  ToggleButtonGroup,
+  ToggleButton,
+} from "react-bootstrap";
 
 function Header() {
   return (
-    <Navbar bg="light" expand="lg" sticky="top">
+    <Navbar bg="light" expand="lg" sticky="top" className="shadow-sm rounded">
       <Container fluid>
         <NavLink className="text-decoration-none" to="/">
           <Navbar.Brand className="fw-semibold">Collections</Navbar.Brand>
         </NavLink>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
-          <Form className="d-flex mx-auto my-2 my-lg-0">
+          <Form className="d-flex mx-auto my-3">
             <FormControl
               type="search"
               placeholder="Search"
@@ -20,25 +28,43 @@ function Header() {
             />
             <Button variant="outline-success">Search</Button>
           </Form>
-          <Form className="d-flex mx-auto my-2 my-lg-0">
-            <Form.Check type="switch" id="switchTheme" label="Dark theme" />
+
+          {/* <Form.Check
+            className="d-flex mx-auto my-3"
+            type="switch"
+            id="switchTheme"
+            label="Dark theme"
+          /> */}
+          <Form className="d-flex mx-auto my-3">
+            <Form.Switch label="Dark theme" />
           </Form>
-          <Form className="d-flex mx-auto my-2 my-lg-0">
-            <Form.Select aria-label="Default select example">
-              <option value="en_US">English</option>
-              <option value="ru">Russian</option>
-            </Form.Select>
-          </Form>
-          <Button variant="success" className="mx-auto my-2 my-lg-0">
+
+          <ToggleButtonGroup
+            className="d-flex mx-auto my-3"
+            name="lang"
+            defaultValue="en_US"
+          >
+            <ToggleButton
+              id="tbg-check-1"
+              value="en_US"
+              variant="outline-secondary"
+            >
+              English
+            </ToggleButton>
+            <ToggleButton
+              id="tbg-check-2"
+              value="ru"
+              variant="outline-secondary"
+            >
+              Russian
+            </ToggleButton>
+          </ToggleButtonGroup>
+
+          <Button variant="success" className="mx-auto my-3">
             Login
           </Button>
         </Navbar.Collapse>
       </Container>
-      {/* <div>LOGO</div>
-      <div>Search</div>
-      <div>Theme</div>
-      <div>Lang</div>
-      <div>Enter</div> */}
     </Navbar>
   );
 }
