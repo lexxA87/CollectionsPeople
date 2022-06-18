59af13d41a8cb7b5f31fa533cb765720f25fbfaf
lexxA87 <alex.o.nikonov@gmail.com>
2022-06-18 21:48:49 +0400

    Added validation for forms login and registration

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 import { Form, FloatingLabel, Button, Modal, Alert } from "react-bootstrap";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string()
+    .max(15, "Must be 15 characters or less")
+    .required("Required"),
+  password: Yup.string()
+    .max(8, "Must be 8 characters or less")
+    .required("Required"),
+  // email: Yup.string().email("Invalid email address").required("Required"),
+});
 
 function LoginForm(props) {
   const { setShowLogin } = props;
@@ -9,25 +21,63 @@ function LoginForm(props) {
         <Modal.Title>Login</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Form className="mb-3">
-          <FloatingLabel
-            controlId="floatingInput"
-            label="Name"
-            className="mb-3"
-          >
-            <Form.Control />
-          </FloatingLabel>
-          <FloatingLabel
-            controlId="floatingPassword"
-            label="Password"
-            className="mb-3"
-          >
-            <Form.Control type="password" />
-          </FloatingLabel>
-          <Button variant="primary" type="submit">
-            Login
-          </Button>
-        </Form>
+        <Formik
+          validationSchema={validationSchema}
+          onSubmit={console.log}
+          initialValues={{
+            name: "",
+            password: "",
+          }}
+        >
+          {({
+            handleSubmit,
+            handleChange,
+            handleBlur,
+            values,
+            touched,
+            isValid,
+            errors,
+          }) => (
+            <Form className="mb-3" noValidate onSubmit={handleSubmit}>
+              <FloatingLabel
+                controlId="floatingInput"
+                label="Name"
+                className="mb-3"
+              >
+                <Form.Control
+                  name="name"
+                  value={values.name}
+                  onChange={handleChange}
+                  isInvalid={!!errors.name}
+                />
+                <Form.Control.Feedback type="invalid" tooltip>
+                  {errors.name}
+                </Form.Control.Feedback>
+              </FloatingLabel>
+
+              <FloatingLabel
+                controlId="floatingPassword"
+                label="Password"
+                className="mb-3"
+              >
+                <Form.Control
+                  type="password"
+                  name="password"
+                  value={values.password}
+                  isInvalid={!!errors.password}
+                  onChange={handleChange}
+                />
+                <Form.Control.Feedback type="invalid" tooltip>
+                  {errors.password}
+                </Form.Control.Feedback>
+              </FloatingLabel>
+
+              <Button variant="primary" type="submit">
+                Login
+              </Button>
+            </Form>
+          )}
+        </Formik>
         <Alert variant="light">
           Or go to{" "}
           <Alert.Link onClick={() => setShowLogin(false)}>
--- src/components/forms/RegistrationForm.jsx
+++ src/components/forms/RegistrationForm.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 import { Form, FloatingLabel, Button, Modal, Alert } from "react-bootstrap";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string()
+    .max(15, "Must be 15 characters or less")
+    .required("Required"),
+  password: Yup.string()
+    .max(8, "Must be 8 characters or less")
+    .required("Required"),
+  email: Yup.string().email("Invalid email address").required("Required"),
+});
 
 function RegistrationForm(props) {
   const { setShowLogin } = props;
@@ -9,30 +21,81 @@ function RegistrationForm(props) {
         <Modal.Title>Registration</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        {" "}
-        <Form className="mb-3">
-          <FloatingLabel controlId="nameInput" label="Name" className="mb-3">
-            <Form.Control />
-          </FloatingLabel>
+        <Formik
+          validationSchema={validationSchema}
+          onSubmit={console.log}
+          initialValues={{
+            name: "",
+            password: "",
+            email: "",
+          }}
+        >
+          {({
+            handleSubmit,
+            handleChange,
+            handleBlur,
+            values,
+            touched,
+            isValid,
+            errors,
+          }) => (
+            <Form className="mb-3" noValidate onSubmit={handleSubmit}>
+              <FloatingLabel
+                controlId="floatingInput"
+                label="Name"
+                className="mb-3"
+              >
+                <Form.Control
+                  name="name"
+                  value={values.name}
+                  onChange={handleChange}
+                  isInvalid={!!errors.name}
+                />
+                <Form.Control.Feedback type="invalid" tooltip>
+                  {errors.name}
+                </Form.Control.Feedback>
+              </FloatingLabel>
 
-          <FloatingLabel
-            controlId="emailInput"
-            label="Email address"
-            className="mb-3"
-          >
-            <Form.Control type="email" />
-          </FloatingLabel>
-          <FloatingLabel
-            controlId="passwordInput"
-            label="Password"
-            className="mb-3"
-          >
-            <Form.Control type="password" />
-          </FloatingLabel>
-          <Button variant="primary" type="submit">
-            Registration
-          </Button>
-        </Form>
+              <FloatingLabel
+                controlId="emailInput"
+                label="Email address"
+                className="mb-3"
+              >
+                <Form.Control
+                  type="email"
+                  name="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  isInvalid={!!errors.email}
+                />
+                <Form.Control.Feedback type="invalid" tooltip>
+                  {errors.email}
+                </Form.Control.Feedback>
+              </FloatingLabel>
+
+              <FloatingLabel
+                controlId="floatingPassword"
+                label="Password"
+                className="mb-3"
+              >
+                <Form.Control
+                  type="password"
+                  name="password"
+                  value={values.password}
+                  isInvalid={!!errors.password}
+                  onChange={handleChange}
+                />
+                <Form.Control.Feedback type="invalid" tooltip>
+                  {errors.password}
+                </Form.Control.Feedback>
+              </FloatingLabel>
+
+              <Button variant="primary" type="submit">
+                Registration
+              </Button>
+            </Form>
+          )}
+        </Formik>
         <Alert variant="light">
           Or go to{" "}
           <Alert.Link onClick={() => setShowLogin(true)}>Login</Alert.Link>
