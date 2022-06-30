3f127ebc1f4d7069f13ab23b16fcd425aaa37bbf
lexxA87 <alex.o.nikonov@gmail.com>
2022-06-30 20:42:19 +0400

    Added card to collection page

--- src/components/collection/CollectionPage.jsx
+++ src/components/collection/CollectionPage.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { Card, Button } from "react-bootstrap";
 
 function CollectionPage() {
   return (
-    <div>
-      <h1 className="text-warning">Collection</h1>
-    </div>
+    <Card className="mb-3">
+      <div className="row g-0">
+        <div className="col-md-4">
+          <Card.Img src="#" />
+        </div>
+        <div className="col-md-8">
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Card.Text>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Text>
+
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </div>
+      </div>
+    </Card>
   );
 }
 
