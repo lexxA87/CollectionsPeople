e4725f676f36e5bc0f137d8f74b53eca2e0afc49
lexxA87 <alex.o.nikonov@gmail.com>
2022-07-10 21:11:23 +0400

    Added entering to admin panel by login, but doesn't work...2

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -1,6 +1,6 @@
 const AdminJS = require("adminjs");
 const AdminJSExpress = require("@adminjs/express");
-const bcrypt = require("bcryptjs");
+// const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 const Collection = require("../models/Collection");
 const ItemCollection = require("../models/ItemCollection");
@@ -44,28 +44,28 @@ const adminJs = new AdminJS({
   rootPath: "/admin",
 });
 
-// const router = AdminJSExpress.buildRouter(adminJs);
+const router = AdminJSExpress.buildRouter(adminJs);
 
-const router = AdminJSExpress.buildAuthenticatedRouter(
-  adminJs,
-  {
-    authenticate: async (email, password) => {
-      const user = await User.findOne({ email });
-      if (user) {
-        const matched = await bcrypt.compare(password, user.password);
-        if (matched) {
-          return user;
-        }
-      }
-      return false;
-    },
-    cookiePassword: "secret-password",
-  },
-  null,
-  {
-    resave: false,
-    saveUninitialized: true,
-  }
-);
+// const router = AdminJSExpress.buildAuthenticatedRouter(
+//   adminJs,
+//   {
+//     authenticate: async (email, password) => {
+//       const user = await User.findOne({ email });
+//       if (user) {
+//         const matched = await bcrypt.compare(password, user.password);
+//         if (matched) {
+//           return user;
+//         }
+//       }
+//       return false;
+//     },
+//     cookiePassword: "secret-password",
+//   },
+//   null,
+//   {
+//     resave: false,
+//     saveUninitialized: true,
+//   }
+// );
 
 module.exports = router;
